24091664eed32c88f241a6c52b9e4f2ce0dbed8f
suchi <suchibankoti95@gmail.com>
2023-01-27 09:32:19 +0530

    get options in random order

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import React from "react"
-import PageOne from "./PageOne"
-
+import { nanoid } from "nanoid";
+import React, { useEffect, useState } from "react";
+import Quiz from "./Ques";
 export default function App() {
-  const [frontPage, setFrontPage] = React.useState(false)
-  function FrontPage() {
-    return (
-      <div className="front-page">
-        <h1 className="quiz-heading">Quizical</h1>
-        <div>
-          <button className="start-quiz-btn" onClick={() => setFrontPage((prev) => !prev)}>Take the Quiz</button>
-        </div>
-      </div>
-    )
-  }
-  return (
-    <div className="page">
-      {frontPage ? <PageOne /> : <FrontPage />}
-    </div>
-  )
-}
+  const [quiz, setQuiz] = useState([]);
+  const [result, setResult] = useState(false);
+  const [i, setI] = useState([]);
 
+  useEffect(() => {
+    if (!result) {
+      async function getQuiz() {
+        const res = await fetch(
+          "https://opentdb.com/api.php?amount=5&type=multiple"
+        );
+        const data = await res.json();
+        setQuiz((prev) => {
+          prev = Array.from(data.results).map((obj) => {
+            const {
+              question: ques,
+              incorrect_answers: incorrect,
+              correct_answer: correct,
+            } = obj;
+            return {
+              id: nanoid(),
+              ques,
+              incorrect,
+              correct,
+              selected: null,
+            };
+          });
+          return prev;
+        });
+        let set1 = new Set();
+        while (set1.size < 4) {
+          set1.add(Math.floor(Math.random() * 4));
+        }
+        setI((prev) => (prev = Array.from(set1.values())));
+      }
+      getQuiz();
+    }
+  }, [result]);
+
+  function select(_id, value) {
+    setQuiz((prev) =>
+      prev.map((obj) => (obj.id === _id ? { ...obj, selected: value } : obj))
+    );
+  }
+  function getResult() {
+    const score = quiz.filter((obj) => obj.selected === obj.correct);
+    return score;
+  }
+
+  const q = quiz.map((obj) => (
+    <Quiz
+      key={nanoid()}
+      Question={obj}
+      select={select}
+      result={result}
+      index={i}
+    />
+  ));
+
+  return (
+    <div>
+      <h1>Quizzical</h1>
+      <ol>{q}</ol>
+      {result ? (
+        <div>
+          score{getResult().length}/{quiz.length}
+        </div>
+      ) : (
+        ""
+      )}
+      <button onClick={() => setResult((prev) => !prev)}>
+        {result ? "New Quiz" : "Get Result"}
+      </button>
+    </div>
+  );
+}
---
+++ src/Ques.js
@@ -0,0 +1,84 @@
+import React from "react";
+import he from "he";
+
+export default function Quiz(props) {
+  const i = props.index;
+  let { id, ques, incorrect, correct, selected } = props.Question;
+  incorrect = [...incorrect, correct].sort();
+
+  const a = incorrect[i[0]];
+  const b = incorrect[i[1]];
+  const c = incorrect[i[2]];
+  const d = incorrect[i[3]];
+  return (
+    <li>
+      {he.decode(ques)}
+      <ul className="question">
+        <li
+          className="option"
+          onClick={() => props.select(id, a)}
+          style={{
+            background:
+              props.result && a === correct
+                ? "green"
+                : selected === a && props.result && a !== correct
+                ? "red"
+                : selected === a && !props.result
+                ? "pink"
+                : "white",
+          }}
+        >
+          {he.decode(a)}
+        </li>
+        <li
+          className="option"
+          onClick={() => props.select(id, b)}
+          style={{
+            background:
+              props.result && b === correct
+                ? "green"
+                : selected === b && props.result && b !== correct
+                ? "red"
+                : selected === b && !props.result
+                ? "pink"
+                : "white",
+          }}
+        >
+          {he.decode(b)}
+        </li>
+        <li
+          className="option"
+          onClick={() => props.select(id, c)}
+          style={{
+            background:
+              props.result && c === correct
+                ? "green"
+                : selected === c && props.result && c !== correct
+                ? "red"
+                : selected === c && !props.result
+                ? "pink"
+                : "white",
+          }}
+        >
+          {he.decode(c)}
+        </li>
+        <li
+          className="option"
+          onClick={() => props.select(id, d)}
+          style={{
+            background:
+              props.result && d === correct
+                ? "green"
+                : selected === d && props.result && d !== correct
+                ? "red"
+                : selected === d && !props.result
+                ? "pink"
+                : "white",
+          }}
+        >
+          {he.decode(d)}
+        </li>
+      </ul>
+    </li>
+  );
+}
